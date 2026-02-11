599c1eeeb5ce8095eeebe997e48586f9b8f82253
Waheguru <dalbirkaur884488@gmail.com>
2026-02-11 13:14:36 -0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,20 @@
+const cacheName = 'diary-v1';
+const assets = ['./', './index.html'];
+
+// Install the service worker and cache files
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// Fetch files from cache if offline
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
+});
